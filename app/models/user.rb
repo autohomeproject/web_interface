fe133bd64e32e9a9067230f4c25558c712eb7d13
Wayne Chang <wayne@neverfear.org>
2012-03-29 16:43:14 -0400

    fixing forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+    @ip_regex = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
+
+
     validates :name, :presence => true
     validates :last_login, :presence => true
     validates :password_hash, :presence => true
-    validates :last_ip, :presence => true
+    validates :last_ip,
+            :presence => true,
+            :format => { :with => @ip_regex }
     validates :phone_number, :presence => true
     has_many :device_permissions
     has_many :conditions
